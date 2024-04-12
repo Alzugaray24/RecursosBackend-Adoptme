6221ddcfc23585a5f50be69539b7e82b40c6c6bc
Matias Alzugaray Piven <alzugaray1997@gmail.com>
2024-04-12 14:24:24 -0300

    configurando handlebars

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import exphbs from "express-handlebars";
+import path from "path"; // Importa el módulo 'path' para trabajar con rutas de archivos
+import __dirname from "./utils/index.js"; // Importa __dirname desde utils/index.js
 
 import usersRouter from "./routes/users.router.js";
 import petsRouter from "./routes/pets.router.js";
@@ -11,8 +14,22 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const connection = mongoose.connect(process.env.MONGO_URL);
 
+// Configura Handlebars como motor de plantillas
+const hbs = exphbs.create({
+  extname: ".hbs",
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "..", "views/layouts"), // Corrige la ruta para los layouts
+});
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "..", "views")); // Utiliza path.join para establecer la ruta de las vistas
+
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("users", {});
+});
 
 app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
