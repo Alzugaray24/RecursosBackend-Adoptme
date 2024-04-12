a48cdcb237354844ee10561fe26d771049cef1f0
Matias Alzugaray Piven <alzugaray1997@gmail.com>
2024-04-12 14:00:57 -0300

    subiendo las vistas

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import exphbs from "express-handlebars";
+import path from "path";
+import __dirname from "./utils/index.js";
 
 import usersRouter from "./routes/users.router.js";
 import petsRouter from "./routes/pets.router.js";
 import adoptionsRouter from "./routes/adoption.router.js";
 import sessionsRouter from "./routes/sessions.router.js";
+import viewsRouter from "./routes/views.routes.js";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
-const connection = mongoose.connect(process.env.MONGO_URL);
+const PORT = process.env.DEV_PORT || 8080;
+const connection = mongoose.connect(process.env.DEV_MONGO_URL);
+
+const hbs = exphbs.create({ extname: "hbs" });
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+
+app.set("views", path.join(__dirname, "..", "views"));
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/", viewsRouter);
 
 app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
